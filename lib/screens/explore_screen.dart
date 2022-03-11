ae4be0112884fec6fabc5edcb7e76b03f4010653
Muhammad Ibraheem <ibrahimjhullan7@gmail.com>
2022-03-11 22:11:19 +0500

    complete Explore screen with two listviews TotalRecipeListView and FrindPostListView

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -9,29 +9,16 @@ class ExploreScreen extends StatelessWidget {
   final MockFooderlichService mockFooderlichService = MockFooderlichService();
   @override
   Widget build(BuildContext context) {
-    // 1
     return FutureBuilder(
-      // 2
       future: mockFooderlichService.getExploreData(),
-      // 3
       builder: (context, AsyncSnapshot<ExploreData> snapshot) {
-        // 4
         if (snapshot.connectionState == ConnectionState.done) {
-          // 5
           return ListView(
-            // 6
             scrollDirection: Axis.vertical,
             children: [
-              // 7
               TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
-              // 8
               const SizedBox(height: 16),
-              // 9
-              // TODO: Replace this with FriendPostListView
-              Container(
-                height: 400,
-                color: Colors.green,
-              ),
+              FriendPostListView(friendPosts: snapshot.data?.friendPosts ?? []),
             ],
           );
         } else {
