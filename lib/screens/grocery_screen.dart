3a5a581d85a55ed2715c1364f182a15b715e4bff
Muhammad Ibraheem <ibrahimjhullan7@gmail.com>
2022-03-14 17:01:27 +0500

    create Grocery manager model

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'empty_grocery_screen.dart';
+import 'package:provider/provider.dart';
+import '../models/models.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
