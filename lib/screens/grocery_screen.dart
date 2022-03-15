af793566113b61e70bc4adfe488350ae1f90ca16
Muhammad Ibraheem <ibrahimjhullan7@gmail.com>
2022-03-15 15:10:51 +0500

    added Grocery Item screen

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -2,12 +2,51 @@ import 'package:flutter/material.dart';
 import 'empty_grocery_screen.dart';
 import 'package:provider/provider.dart';
 import '../models/models.dart';
+import 'grocery_item_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const EmptyGroceryScreen();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          print("floating action button is pressed");
+          final manager = Provider.of<GroceryManager>(context, listen: false);
+
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => GroceryItemScreen(
+                onCreate: (item) {
+                  manager.addItem(item);
+
+                  Navigator.pop(context);
+                },
+                onUpdate: (item) {},
+              ),
+            ),
+          );
+        },
+      ),
+      body: buildGroceryScreen(),
+    );
   }
+}
+
+Widget buildGroceryScreen() {
+  return Consumer<GroceryManager>(
+    builder: (context, manager, child) {
+      if (manager.groceryItems.isNotEmpty) {
+        // TODO 25: Add GroceryListScreen
+        return Container(
+          color: Colors.brown,
+        );
+      } else {
+        return const EmptyGroceryScreen();
+      }
+    },
+  );
 }
